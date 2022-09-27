203b99bfbf4265bdd646c98368b3f169ddd10d34
Himansumallick <dreamhimansu@gmail.com>
2022-09-27 09:34:59 +0545

    navbar changes

--- hms-frontend/src/components/Navbar.js
+++ hms-frontend/src/components/Navbar.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            GALAXY
+            <i class="fa-sharp fa-solid fa-hotel"></i>
+            RESORT
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i
+              className={
+                click ? "fa-solid fa-xmark" : "fa-solid fa-bars-staggered"
+              }
+            />
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/services"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/signIn"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/signUp"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
